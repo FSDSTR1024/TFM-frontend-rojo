f461231827912a894089837a0bb48ffe93bf309d
Jose Peanilla <josepeanilla@hotmail.com>
2025-02-03 19:54:16 +0100

    Modification so that the profile modification form is updated correctly and shows the current data instead of the old one

--- src/components/organisms/Profile/Winery/WineryProfileForm.jsx
+++ src/components/organisms/Profile/Winery/WineryProfileForm.jsx
@@ -1,8 +1,14 @@
+import { useContext } from "react"
+import { AuthContext } from "/src/context/AuthContext"
 import { ProfileForm } from "/src/components/molecules/Profile/Form"
 
-export const WineryProfileForm = ({ user, setToken, navigate, logger }) => {
+export const WineryProfileForm = ({ user, navigate, logger }) => {
+  const { setUser } = useContext(AuthContext) 
+
   const handleOnSubmit = async (formData) => {
     try {
+      logger.debug("Enviando datos al backend:", formData) 
+
       const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/users/wineries/${user.id}`, {
         method: "PUT",
         headers: {
@@ -13,13 +19,25 @@ export const WineryProfileForm = ({ user, setToken, navigate, logger }) => {
       })
 
       const jsonData = await response.json()
+      logger.debug("Respuesta del backend:", jsonData) 
+
       if (!response.ok) throw jsonData
 
       alert("[SUCCESS] Perfil actualizado correctamente!")
+
+      setUser((prevUser) => ({ 
+        ...prevUser, 
+        ...formData, 
+        id: prevUser.id, 
+        _id: prevUser._id 
+      }))
+
+      logger.info("Perfil actualizado con éxito")
+      
       navigate("/")
     } catch (err) {
-      logger.error(err.msg, err.error)
-      alert(`[ERROR] ${err.msg}`)
+      logger.error("Error en la actualización del perfil:", err)
+      alert(`[ERROR] ${err?.msg || "Error desconocido"}`)
     }
   }
 
