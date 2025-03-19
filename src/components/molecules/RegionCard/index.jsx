34ab99afacd898bb6ad4c09c6f2e84e8bb94a521
JosePeanilla <josepeanilla@hotmail.com>
2025-03-19 20:49:02 +0100

    Revert "Wine 045 f nr"

--- src/components/molecules/RegionCard/index.jsx
+++
@@ -1,6 +0,0 @@
-import { RegionCard } from "./RegionCard.jsx"
-import { RegionCardImage } from "./RegionCardImage.jsx"
-import { RegionCardContent } from "./RegionCardContent.jsx"
-import { RegionCardActions } from "./RegionCardActions.jsx"
-
-export { RegionCard, RegionCardImage, RegionCardContent, RegionCardActions }
